d00fe7126c9c67ce812368bafb039b86f7897d00
mohammad garmabi <49670466+mwmdgmb@users.noreply.github.com>
2020-02-17 11:32:33 +0330

    added : delete post get and patch

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 // import { getCookieFromRequest } from './../helpers/utils';
 
+const axiosInstance = axios.create({
+	baseURL: 'http://localhost:3000/api/v1'
+});
+
 const setAuthHeader = () => {
 	const token = Cookies.getJSON('jwt');
 
@@ -12,10 +16,49 @@ const setAuthHeader = () => {
 	return undefined;
 };
 
+const rejectPromise = (resError) => {
+	let error = {};
+
+	if (resError && resError.response && resError.response.data) {
+		error = resError.response.data;
+	} else {
+		error = resError;
+	}
+
+	return Promise.reject(error);
+};
+
 export const getSecretData = async () => {
-	return await axios.get('/api/v1/secret', setAuthHeader()).then((response) => response.data);
+	return await axiosInstance.get('/secret', setAuthHeader()).then((response) => response.data);
 };
 
 export const getSecretDataServer = async (req) => {
-	return await axios.get('http://localhost:3000/api/v1/secret', setAuthHeader(req)).then((response) => response.data);
+	return await axiosInstance.get('/secret', setAuthHeader(req)).then((response) => response.data);
+};
+
+export const getPortfolios = async () => {
+	return await axiosInstance.get('/portfolios').then((response) => response.data);
+};
+
+export const createPortfolio = async (portfolioData) => {
+	return await axiosInstance
+		.post('/portfolios', portfolioData, setAuthHeader())
+		.then((response) => response.data)
+		.catch((error) => rejectPromise(error));
+};
+
+export const updatePortfolio = async (portfolioData) => {
+	return await axiosInstance
+		.patch(`/portfolios/${portfolioData._id}`, portfolioData, setAuthHeader())
+		.then((response) => response.data)
+		.catch((error) => rejectPromise(error));
+};
+
+export const getPortfolioById = async (id) => {
+	console.log('Index Get Function');
+	return await axiosInstance.get(`/portfolios/${id}`, setAuthHeader()).then((response) => response.data);
+};
+
+export const deletePortfolio = async (portfolioId) => {
+	return await axiosInstance.delete(`/portfolios/${portfolioId}`, setAuthHeader()).then((response) => response.data);
 };
