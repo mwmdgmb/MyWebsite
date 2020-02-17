104ff6dbee491db66f62b1a78db6f20bbc2d179c
mohammad garmabi <49670466+mwmdgmb@users.noreply.github.com>
2020-02-17 11:40:08 +0330

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const WithCss = require('@zeit/next-css');
 const WithSass = require('@zeit/next-sass');
 const withImages = require('next-images');
+const withFonts = require('next-fonts');
 
-module.exports = withImages(WithCss(WithSass()));
+module.exports = withImages(withFonts(WithCss(WithSass())));
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const routes = require('next-routes');
 
 module.exports = routes()
-    // .add('about')
-    .add('profile', '/profile/:id');
-	// .add('user', '/user/:id', 'profile')
-	// .add('/:noname/:lang(en|es)/:wow+', 'complex')
-	// .add({ name: 'beta', pattern: '/v3', page: 'v3' });
+	// .add('about')
+	.add('portfolio', '/portfolio/:id')
+	.add('portfolioEdit', '/portfolios/:id/edit');
+// .add('user', '/user/:id', 'profile')
+// .add('/:noname/:lang(en|es)/:wow+', 'complex')
+// .add({ name: 'beta', pattern: '/v3', page: 'v3' });
